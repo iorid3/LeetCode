475b294b3cccf7960f5aa56a164405e140464109
iorid3 <itakeshita@my.bcit.ca>
2023-08-02 11:45:46 -0700

    subarray

--- subarray.ts
+++ subarray.ts
@@ -1,24 +1,27 @@
 function longestSubarray(nums: number[]): number {
-    let maxCount = 0; // 最長の非空の連続する1のみから成る部分配列のサイズを格納する変数
-    let currentCount = 0; // 現在の連続する1の数を格納する変数
-    let hasZero = false; // 配列に0が含まれているかを示すフラグ
+    let maxCount = 0;
+    let currentCount = 0;
+    let prevCount = 0;
+    let hasZero =false
   
     for (let i = 0; i < nums.length; i++) {
       if (nums[i] === 1) {
         currentCount++;
       } else {
-        // 0が現れた場合
-        hasZero = true;
-        maxCount = Math.max(maxCount, currentCount); // 現在の最大の連続する1の数を更新
-        currentCount = 0; // 現在の連続する1の数をリセット
+        prevCount = currentCount;
+        currentCount = 0;
+        hasZero = true
       }
+  
+      maxCount = Math.max(maxCount, prevCount + currentCount);
     }
-  
-    // 最後に配列の末尾が1で終わる場合も考慮して、最大の連続する1の数を更新
-    maxCount = Math.max(maxCount, currentCount);
-  
-    // 配列に0が含まれている場合、1つの要素を削除することにより連続する1のみから成る部分配列のサイズを求める
-    // ただし、連続する1のみから成る部分配列が存在しない場合は0を返す
-    return hasZero ? maxCount : maxCount - 1;
+
+    if(hasZero){
+      return maxCount > 0 ? maxCount : 0;
+    }
+    else{
+       return maxCount > 0 ? maxCount-1 : 0;
+    }
   }
-  
+
+longestSubarray([1.0,1,1])
